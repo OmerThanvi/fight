06717c47700bfc1a554e3aa0fb8bd21625e402b9
OmerThanvi <omer.thanvi@elev.ga.ntig.se>
2026-09-24 10:45:36 +0200

    tytyjuygtfgytu

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,32 +8,71 @@ string namn1 =  Console.ReadLine();
 
 string namn2 = Console.ReadLine();
 
-Console.WriteLine($"Hej {namn1} och {namn2}");
+Console.WriteLine($"Hej {namn1} och {namn2}. Tryck 1 för låg skada eller valfri knapp för högre skada");
 
 while (hp > 0 && hp2 > 0)
 {
-    Console.WriteLine("båda lever");
-   int skada = Random.Shared.Next(10, 20);
-   int skada2 = Random.Shared.Next(10, 20);
-   hp = Math.Max(0, hp - skada);
-   hp2 = Math.Max(0, hp2 - skada2);
-   Console.WriteLine($"{namn1} har {hp} kvar och {namn2} har {hp2} kvar");
-    
+   string Val1 = Console.ReadLine();
+
+   int skada = 0;
+
+   if (Val1 == "1")
+    {
+        skada = Random.Shared.Next(10,19);
+        Console.WriteLine($"{namn1} använde attack 1");
+    }
+    else
+    {
+        skada = Random.Shared.Next(27,32);
+        Console.WriteLine($"{namn1} använde attack 2");
+    }
+    hp2-=skada;
+    Console.WriteLine($"{namn2} förlorade {skada} hp och har {hp2} kvar");
+
+    if (hp2<=0)
+    {
+        break;
+    }
+
+    Console.WriteLine($"Det är {namn2}s tur! (HP: {hp2})");
+    Console.WriteLine("Välj attack: Skriv 1 för Låg skada, eller valfri knapp för Hög skada");
+    string val2 = Console.ReadLine();
+
+    if (val2 == "1")
+    {
+        skada = Random.Shared.Next(10,19);
+        Console.WriteLine($"{namn2} använde attack 1");
+    }
+
+    else
+    {
+        skada = Random.Shared.Next(27,32);
+        Console.WriteLine($"{namn2} använde attack 2");
+    }
+    hp-=skada;
+    Console.WriteLine($"{namn1} förlorade {skada} hp och har {hp} kvar");
+
+    if(hp<=0)
+    {
+        break;
+    }
+    Console.WriteLine($"Det är {namn1}s tur! (HP: {hp})");
+    Console.WriteLine("Välj attack: Skriv 1 för Låg skada, eller valfri knapp för Hög skada");
 }
 
 
-if (hp > 0 && hp2 == 0)
-{
-    Console.WriteLine($"{namn1} vann");
-}
-else if(hp == 0 && hp2 > 0)
-{
-    Console.WriteLine($"{namn2} vann");
-}
+ if (hp > 0 && hp2 == 0)
+ {
+     Console.WriteLine($"{namn1} vann");
+ }
+ else if(hp == 0 && hp2 > 0)
+ {
+     Console.WriteLine($"{namn2} vann");
+ }
 
-else if (hp == 0 && hp2 == 0)
-{
-    Console.WriteLine("oavgjort");
-}
+ else if (hp == 0 && hp2 == 0)
+ {
+Console.WriteLine("oavgjort");
 
 Console.ReadLine();
+ }
